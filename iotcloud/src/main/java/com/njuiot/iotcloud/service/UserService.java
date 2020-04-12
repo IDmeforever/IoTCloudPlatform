649eddf818fd9b1e1d02f9696aaa8c13c1f5af33
Zhang <idmeforever@gmail.com>
2020-04-12 21:46:06 +0800

    <feat>: <add database interface in server>

---
+++ iotcloud/src/main/java/com/njuiot/iotcloud/service/UserService.java
@@ -0,0 +1,23 @@
+package com.njuiot.iotcloud.service;
+
+
+
+import com.njuiot.iotcloud.mapper.UserMapper;
+import com.njuiot.iotcloud.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author:wjup
+ * @Date: 2018/9/26 0026
+ * @Time: 15:23
+ */
+@Service
+public class UserService {
+    @Autowired
+    UserMapper userMapper;
+
+    public User Sel(int id) {
+        return userMapper.Sel(id);
+    }
+}
